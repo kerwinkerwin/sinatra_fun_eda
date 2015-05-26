bbde4aa134d2ba6eca0adad3aaac1d253a455727
Raquel Moss <raquelxmoss@gmail.com>
2015-05-27 11:15:56 +1200

    working solution

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,10 @@
 get '/' do
- erb :index
+  #Root
+  if session[:user]!= nil
+    erb :index
+  else
+    redirect "/sign_in"
+  end
 end
 
 get '/sign_in' do
@@ -11,16 +16,28 @@ get '/sign_up' do
 end
 
 post '/sign_up' do
-
+  user = User.create(email:params[:email], password:params[:password])
+  session[:user] = user.id
+  redirect "/"
 end
 
 post '/sign_in' do
-
+  puts params[:password]
+  user = User.find_by(email: params[:email])
+    if user
+      if user.password == params[:password]
+        session[:user] = user.id
+        redirect "/"
+      end
+    end
+    @error ="wrong credentials"
+        erb :sign_in
 end
 
 get '/sign_out' do
-
+  erb :sign_out
 end
 post '/sign_out' do
-
+  session.delete(:user)
+  redirect "/"
 end
